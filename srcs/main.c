5db09c04537bb25622f9aaa8ef67adb7df723c86
Aurore Roma <aroma@student.42.fr>
2022-03-18 12:46:08 +0100

    .

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: aroma <aroma@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 20:12:12 by pblagoje          #+#    #+#             */
-/*   Updated: 2022/03/18 12:45:19 by aroma            ###   ########.fr       */
+/*   Updated: 2022/03/18 12:46:02 by aroma            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,6 @@ int	main(int ac, char **av, char **envp)
 	char	*line;
 	t_shell	shell;
 
-	(void)av;
 	if (ac != 1)
 		exit(0);
 	init_shell(&shell);
